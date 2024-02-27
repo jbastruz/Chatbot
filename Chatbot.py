49baf9a0cdd3d8e1504e919af4df213a67815b59
jbastruz <sv2ptmp0@duck.com>
2024-02-27 15:21:01 +0100

    adding model seletor

--- Chatbot.py
+++ Chatbot.py
@@ -8,6 +8,7 @@ import streamlit as st  # Import the streamlit library for creating web apps
 import yaml  # Import the yaml library for working with YAML files
 import os  # Import the os module for interacting with the operating system
 from dotenv import load_dotenv
+from streamlit_option_menu import option_menu
 
 UPLOAD_DIRECTORY = os.path.abspath("Data")
 load_dotenv()
@@ -31,8 +32,8 @@ if not os.getenv("MISTRAL_API_KEY"):
 else :
     mistral_api_key = os.getenv("MISTRAL_API_KEY")
 
+models = {"Mistral-tiny":"open-mistral-7b", "Mistral-small":"open-mixtral-8x7b", "Mistral-medium":"mistral-medium-2312", "Mistral-large (WIP)":"mistral-large-latest"}
 model = "open-mixtral-8x7b"
-
 client = MistralClient(api_key=mistral_api_key)
 
 name, authentication_status, username = authenticator.login('main', fields = {'Form name':'Chatbot ASTRUZ', 'Username':'Nom d\'utilisateur', 'Password':'Mot de passe', 'Login':'Connexion'})
@@ -41,6 +42,11 @@ if authentication_status:
     with st.sidebar:
         st.title("💬 Chatbot ASTRUZ")
         st.caption("By Jean-Baptiste ASTRUZ")
+        st.header("Modèles:")
+        selector = option_menu(None ,["Mistral-tiny", 'Mistral-small', 'Mistral-medium', 'Mistral-large (WIP)'], 
+            icons=['star', 'star-half', 'star-fill', 'stars'], menu_icon="chat-dots", default_index=1)
+        st.divider()
+        st.header("Historique de conversation:")
 
     if "messages" not in st.session_state:
         st.session_state["messages"] = [{"role": "assistant", "content": f"Bonjour {name}, comment puis-je vous aider?"}]
@@ -55,13 +61,13 @@ if authentication_status:
         st.session_state.history.append(ChatMessage(role= "user", content= prompt))
 
         st.chat_message("user").write(prompt)
-        
+
         with st.chat_message("assistant"):
             message_placeholder = st.empty()
             full_response = ""
             print(st.session_state.history)
             for response in client.chat_stream(
-                model=model,
+                model=models[selector],
                 messages=st.session_state.history
             ):
                 full_response += (response.choices[0].delta.content or "")
